bd396cf801a596ca9da125fc9b5da53cd96e13dd
Nikita Goryachev <nic.gor@yandex.ru>
2022-11-09 07:25:01 +0300

    Play with CSS

--- app/views.py
+++ app/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+
+def question(request):
+    return render(request, 'question.html')
--- askme/urls.py
+++ askme/urls.py
@@ -20,5 +20,6 @@ from app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index),
+    path('', views.index, name='index'),
+    path('question', views.question, name='question')
 ]
