b491782f89cae3a28ae137f4176c42da94f1cec8
Nikita Goryachev <nic.gor@yandex.ru>
2022-11-09 07:29:05 +0300

    Add urls to main page

--- askme/urls.py
+++ askme/urls.py
@@ -20,6 +20,6 @@ from app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index'),
+    path('', views.index, name='main'),
     path('question', views.question, name='question')
 ]
