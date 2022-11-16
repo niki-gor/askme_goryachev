c8de453c9a6062796fc8c67139194adedc5ee2dc
Nikita Goryachev <nic.gor@yandex.ru>
2022-11-16 15:08:11 +0300

    Make migrations

--- app/models.py
+++ app/models.py
@@ -1,5 +1,37 @@
 from django.db import models
 
+
+class User(models.Model):
+    login = models.CharField(max_length=30)
+    password_hash = models.CharField(max_length=64)
+    nickname = models.CharField(max_length=30)
+    avatar_url = models.CharField(max_length=100)
+
+
+class Post(models.Model):
+    pass
+
+
+class Question(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.PROTECT)
+    asker = models.ForeignKey(User, on_delete=models.PROTECT)
+    name = models.CharField(max_length=70)
+    description = models.CharField(max_length=140)
+
+
+class Answer(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.PROTECT)
+    question = models.ForeignKey(Question, on_delete=models.PROTECT)
+    commentator = models.ForeignKey(User, on_delete=models.PROTECT)
+    text = models.CharField(max_length=140)
+
+
+class LikeCounter(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.PROTECT)
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    value = models.IntegerField()
+
+
 questions = {
     question_id: {
         "question_id": question_id,
